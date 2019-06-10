541b0739afc83ccf305af706ed351f806b0bb941
Paul Sinclair <sinclairpaul@hotmail.com>
2019-06-10 16:53:31 -0400

    :hammer: Cleanup and fix laravel.log

--- bookstack/rootfs/etc/cont-init.d/bookstack.sh
+++ bookstack/rootfs/etc/cont-init.d/bookstack.sh
@@ -29,9 +29,6 @@ if ! bashio::fs.file_exists "/data/bookstack/appkey.txt"; then
 	key=$(php /var/www/bookstack/artisan key:generate --show)
 	echo "${key}" > /data/bookstack/appkey.txt
 	bashio::log.info "App Key generated: ${key}"
-#else
-#	key=$(cat /data/bookstack/appkey.txt)
 fi
-#sed -i "s,APP_KEY=SomeRandomString,APP_KEY=${key},g" \
-#     /var/www/bookstack/.env
+ln -sf /dev/stderr /var/www/bookstack/storage/logs/laravel.log
 
