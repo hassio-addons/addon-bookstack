a7c87d3a74cb935c637a4ab6a5813bcbd7708cb3
ajesseperez <20435966+ajesseperez@users.noreply.github.com>
2021-04-21 18:12:58 -0500

    Update nginx.sh

--- bookstack/rootfs/etc/cont-init.d/nginx.sh
+++ bookstack/rootfs/etc/cont-init.d/nginx.sh
@@ -4,13 +4,12 @@
 # This file configures nginx
 # ==============================================================================
 
-if bashio::var.has_value "$(bashio::addon.port 80)"; then
-    bashio::config.require.ssl
-    bashio::var.json \
-        certfile "$(bashio::config 'certfile')" \
-        keyfile "$(bashio::config 'keyfile')" \
-        ssl "^$(bashio::config 'ssl')" \
-        | tempio \
-            -template /etc/nginx/templates/direct.gtpl \
-            -out /etc/nginx/servers/direct.conf
-fi
+bashio::config.require.ssl
+bashio::var.json \
+    certfile "$(bashio::config 'certfile')" \
+    keyfile "$(bashio::config 'keyfile')" \
+    ssl "^$(bashio::config 'ssl')" \
+    | tempio \
+        -template /etc/nginx/templates/direct.gtpl \
+        -out /etc/nginx/servers/direct.conf
+
