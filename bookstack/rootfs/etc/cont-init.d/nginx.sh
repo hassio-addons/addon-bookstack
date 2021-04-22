b00134fbc00a3d8777d7798b87f329eb06cad419
ajesseperez <20435966+ajesseperez@users.noreply.github.com>
2021-04-22 16:18:33 -0500

    ✨ Update nginx.sh to remove if Statement (#76)
    * Update nginx.sh
    
    * Update NGINX Script to log if no external port is specified

--- bookstack/rootfs/etc/cont-init.d/nginx.sh
+++ bookstack/rootfs/etc/cont-init.d/nginx.sh
@@ -4,13 +4,16 @@
 # This file configures nginx
 # ==============================================================================
 
-if bashio::var.has_value "$(bashio::addon.port 80)"; then
-    bashio::config.require.ssl
-    bashio::var.json \
-        certfile "$(bashio::config 'certfile')" \
-        keyfile "$(bashio::config 'keyfile')" \
-        ssl "^$(bashio::config 'ssl')" \
-        | tempio \
-            -template /etc/nginx/templates/direct.gtpl \
-            -out /etc/nginx/servers/direct.conf
+if bashio::var.is_empty "$(bashio::addon.port 80)"; then
+    bashio::log.warning "No host port is configured, please ensure a port is set for external access to function"
 fi
+
+bashio::config.require.ssl
+bashio::var.json \
+    certfile "$(bashio::config 'certfile')" \
+    keyfile "$(bashio::config 'keyfile')" \
+    ssl "^$(bashio::config 'ssl')" \
+    | tempio \
+        -template /etc/nginx/templates/direct.gtpl \
+        -out /etc/nginx/servers/direct.conf
+
